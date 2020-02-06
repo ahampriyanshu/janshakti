33593bb41c2378d6def1b42a2a1afbc1cf3e64cc
PriyanshuMay <tiwarimay2002@gmail.com>
2020-02-07 04:54:55 +0530

    comitted to Hackdata

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+require("PHPMailer/class.phpmailer.php");
+$mailer = new PHPMailer();
+$mailer->IsSMTP();
+$mail->SMTPDebug = 1; 
+$mailer->Host = 'ssl://smtp.gmail.com';
+$mailer->Port = 587; //can be 587
+$mailer->SMTPAuth = TRUE;
+// Change this to your gmail address
+$mailer->Username = 'priyanshootiwari@gmail.com';  
+// Change this to your gmail password
+$mailer->Password = 'Lexdumbo12';  
+// Change this to your gmail address
+$mailer->From = 'priyanshutiwari@gmail.com';  
+// This will reflect as from name in the email to be sent
+$mailer->FromName = 'Priyanshu Tiwari'; 
+$mailer->Body = 'If you are receving this mail then phpmailer is sucessfully working';
+$mailer->Subject = 'test case of phpmailer';
+// This is where you want your email to be sent
+$mailer->AddAddress('vaibhav2017shukla@gmail.com');  
+if(!$mailer->Send())
+{
+    echo "Message was not sent<br/ >";
+    echo "Mailer Error: " . $mailer->ErrorInfo;
+}
+else
+{
+    echo "Message has been sent";
+}
+?>
