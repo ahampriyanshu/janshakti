b283c9a091215321d45a580cc53e2df5d9a51cd5
PriyanshuMay <tiwarimay2002@gmail.com>
2020-02-08 02:55:16 +0530

    corrected view complaints

--- viewcomplaint.php
+++ viewcomplaint.php
@@ -15,14 +15,107 @@ include("essentials/database.php");
   <meta name="keywords" content="gate,priyanshumay,gne,gndec,">
   <meta name="author" content="PriyanshuMay,priyanshumay">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add answer</title>
+    <title>View Complaint</title>
     <link href="forum.css" rel="stylesheet" type="text/css">
     <style type="text/css">
-    body{
-    padding-top: 100px;
-    overflow-x: hidden;
-    padding-left: 130px;
+        @media only screen and (max-width:480px ) {
+    html,  body{
+        margin:0;
+        height: 100%;
+        padding-right: 2%;
+        padding-left: 2%;
+        overflow-x: hidden;
+        background-color:#f3f7f7 ;
+        }
+        body::-webkit-scrollbar {
+        display: none;
     }
+    .question_display {
+    position: relative;
+    border: none;
+    width: 90%;
+    }
+    #answerbox{
+    position: relative;;
+    width: 90%;
+    }
+    .reply{
+width: 90%;
+font-size: 15px;
+line-height: 1.4;
+min-height: 20px;
+border: 1px solid #e2e2e2;
+box-shadow: 0 0 5px #888;
+border-radius: 4px;
+    }
+    #submitans {
+    background-color:#833AB4;
+    color: white;
+    padding: 11px;
+    font-size: 11px;
+    border: none;
+    cursor: pointer;
+    font-weight: bold;
+    font-family: courier;
+    border-radius: 5%;
+    }
+    #submitans:hover, #submitans:focus {
+    background-color: #DB4437;
+    }
+    #title{
+    line-height: 1.5;
+    letter-spacing: -1px;
+    color: #333;
+    tab-size: 4;
+    word-break: break-word;
+    text-align: left;
+    direction: ltr;
+    user-select: text;
+    font-size: 25px;
+    font-family: Courier new ;
+    }
+    .select {
+    position: relative;
+    letter-spacing: 0px;
+    font-family: courier;
+    color: red;
+    }
+    .select option{
+    font-family: courier;
+    color: white;
+    font-weight: bold;
+    }
+    .option{
+    font-family: courier;
+    color: white;
+    font-weight: bold;
+    background-color:#833AB4;
+    padding: 5px;
+    
+    }
+    #specs{
+    font-size:12px;
+    font-family: courier new ;
+    font-weight: bold;
+    color: #833AB4;
+    }
+    #details{
+    font-weight: bold;
+    color: red;
+    font-size:12px;
+    }
+    #line{
+    border: 2px solid red;
+    border-radius: 5px;
+    }
+}
+    @media only screen and (min-width:481px ) {
+
+html, body {
+  background-color: #f3f7f7;
+  overflow-x: hidden;
+  padding-left: 100px;
+}
     .question_display {
     position: relative;
     border: none;
@@ -94,6 +187,7 @@ include("essentials/database.php");
     border: 2px solid red;
     border-radius: 5px;
     }
+}
     </style>
   </head>
   <body>
@@ -111,34 +205,41 @@ include("essentials/database.php");
       <span id="specs">question comes under</span> &nbsp;<span id="details"><?php echo $row["branch"]; ?></span><span id="specs"> branch</span>&emsp;
       <span id="specs">posted on</span> &nbsp;<span id="details"><?php echo $row["datetym"]; ?></span><br>
     </div><br><br>
-    <?php
-    endwhile;
-    ?>
+    <button onclick="myFunction()">Show</button>
+     <?php
+        echo '<div class="content" id="content">';
+            $queid = $row["id"];
+            $ql = "SELECT * FROM answers WHERE id = '$queid' ORDER BY datetym DESC ";
+            $resul = $con->query($ql);
+            if ($resul->num_rows > 0) {
+            while($ro = $resul->fetch_assoc()) {
+            echo "<br><div id='answer_box'>
+                <span id='anstitle'><br> ". $ro["content"] . "</span><br><span id='specs'> <br>&emsp;&emsp;&emsp;Time required is </span>&nbsp;<span id='details'> " . $ro ["tym"] . "&emsp;&emsp;<span id='specs'> Difficulty Level according to user is </span>&nbsp; ". $ro["level"] ."&emsp;&emsp;<span id='specs'> Answered by </span>&nbsp; " . $ro["username"] . "&emsp;&emsp;<span id='specs'> answered on </span>&nbsp; " . $ro["datetym"] ."</span></div>";
+                }
+                } else {
+                echo "<br><div id='answer_box'><span id='anstitle'>Be the first to dicuss</span></div>";
+                }
+            echo '<br><br></div>';
+            endwhile; ?>
     <div id="answerbox" >
     <form name="addform" action="postans.php" method="POST">
         <input type="hidden" name="qid" value="<?php echo $ID;?>">
-        <textarea name="content" class="input_text" id="content" placeholder="Enter the solution" cols="100" rows="10" required></textarea>
+        <textarea name="content" class="reply" name="reply" placeholder="Do you realte to the problem ?" cols="100" rows="10" required></textarea>
         <div class="select">
           <br>
-          <label>Difficulty level</label>
-          <select name="level" class="option">
-            <option value="Low">Low</option>
-            <option value="Medium">Medium</option>
-            <option value="High">High</option>
-          </select>&emsp;&emsp;
-          <label>Time Required</label>
-          <select name="tym" class="option">
-            <option value="0-2 min">0-2 Min</option>
-            <option value="2-5 Min">2-5 Min</option>
-            <option value="5-10 Min">5-10 Min</option>
-          </select>
-          &emsp;&emsp;&emsp;&emsp;<button type="submit" name="submit" class="submit" id="submitans" >Post Your Answer</button>
+          <button type="submit" name="submit" class="submit" id="submitans" >Post Reply</button>
         </div>
  
- <script src="essentials/ckeditor/ckeditor.js" ></script>    
- <script type="text/javascript">
-     CKEDITOR.replace('content');
- </script>   
+ <script>
+function myFunction() {
+  var x = document.getElementById("content");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
       </form>
     </div>
   </div>
